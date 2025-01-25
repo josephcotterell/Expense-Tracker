05a1099764fdd12915dd09b239b5ab2d70e23422
joseph <josephcotterell@hotmail.co.uk>
2025-01-25 15:20:04 +0000

    added Transaction list componnent

---
+++ my-app/src/components/TransactionList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const TransactionList = () => {
+  <>
+    <h4>History</h4>
+    <ul id="list" class="list">
+      <li class="minus">
+        Cash <span>-£500</span>
+        <button class="delete-btn">x</button>
+      </li>
+    </ul>
+  </>;
+};
