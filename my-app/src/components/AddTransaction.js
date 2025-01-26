04e007273ea092f0ac1c0d4b24a1c4a517ef7eec
joseph <josephcotterell@hotmail.co.uk>
2025-01-26 22:30:49 +0000

    used useState hook to amount and text values

--- my-app/src/components/AddTransaction.js
+++ my-app/src/components/AddTransaction.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState("0");
   return (
     <>
       <h3>Add Transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter text...." />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text...."
+          />
         </div>
         <div className="form-control">
           <label for="amount">
@@ -16,7 +23,12 @@ export const AddTransaction = () => {
             (negative - expense, positive - income)
           </label>
 
-          <input type="number" placeholder="Enter amount...." />
+          <input
+            type="number"
+            value={amount}
+            setAmount={(e) => setText(e.target.value)}
+            placeholder="Enter amount...."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
