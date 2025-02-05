0cba3e1da49efad9f68fd1517a45ed2dbba29c7a
joseph <josephcotterell@hotmail.co.uk>
2025-02-05 21:37:57 +0000

    created app reducer to specifiy state changes

--- my-app/src/components/TransactionList.js
+++ my-app/src/components/TransactionList.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
   return (
     <>
       <h4>History</h4>
       <ul class="list">
-        <li className="minus">
-          Cash <span>-£500</span>
-          <button class="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <li className="minus">
+            {transactioan.text} <span>-£500</span>
+            <button class="delete-btn">x</button>
+          </li>
+        ))}
       </ul>
     </>
   );
