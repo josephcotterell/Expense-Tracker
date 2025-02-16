6a8668cba76aa7b1df12b0ab9a753ac74a0d5f4b
joseph <josephcotterell@hotmail.co.uk>
2025-02-16 18:05:00 +0000

    fixed white screen error and created componnent to change colour of the borders when the balance is plus or minus

--- my-app/src/components/Transaction.js
+++ my-app/src/components/Transaction.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? "-" : "+";
   return (
-    <li className="minus">
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
-        {sign}${Maths.abs(transaction.amount)}
+        {sign}£{Math.abs(transaction.amount)}
       </span>
       <button class="delete-btn">x</button>
     </li>
