61e67310cb28a2c9ca5e5153426ed5b107e720e1
joseph <josephcotterell@hotmail.co.uk>
2025-03-07 22:07:01 +0000

    used reduced to calculate changes in amounts

--- my-app/src/components/IncomeExpenses.js
+++ my-app/src/components/IncomeExpenses.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amount.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
   return (
     <div className="inc-exp-container">
       <div>
         <h3>Income</h3>
-        <p class="money plus">+0.00</p>
+        <p class="money plus">{income}</p>
       </div>
       <div>
         <h3>Expense</h3>
-        <p class="money minus">-£0.00</p>
+        <p class="money minus">{expense}</p>
       </div>
     </div>
   );
