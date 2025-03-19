2c0cf436731dafd4b0863e7ac671d5a0162af724
joseph <josephcotterell@hotmail.co.uk>
2025-03-19 18:54:12 +0000

    created a delete function

--- my-app/src/context/GlobalState.js
+++ my-app/src/context/GlobalState.js
@@ -11,14 +11,23 @@ const initialState = {
 };
 
 export const GlobalContext = createContext(initialState);
-
+// Provider
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  /// Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
 
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        deleteTransaction,
       }}
     >
       {children}
